9b171833afd056723442c912cd635f6eb6c53b4f
SK ARIF HOSSAIN <hossainarif8364@gmail.com>
2026-03-17 13:17:20 +0530

    Update CORS configuration to allow all origins

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+    origin:*,
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
