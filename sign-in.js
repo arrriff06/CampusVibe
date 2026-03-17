3d9d9187d3f334e020f0a32e8d8fe56f0710bef4
SK ARIF HOSSAIN <hossainarif8364@gmail.com>
2026-03-17 14:05:51 +0530

    Update API endpoint for login request

--- sign-in.js
+++ sign-in.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const password = document.getElementById("password").value;
 
     try {
-      const res = await fetch("https://campusvibe-backend.onrender.com/api/auth/login", {
+     const res = await fetch("https://campusvibe-8s7a.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
